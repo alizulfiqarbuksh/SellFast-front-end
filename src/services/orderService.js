f6e113fbc50460874b661ed89cd88770402b62e9
AhmedJ8000 <ahmedjamal8000@gmail.com>
2026-01-26 14:26:46 +0300

    Added Order JSX and it's service

---
+++ src/services/orderService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/orders`
+
+const show = async () => {
+  try {
+
+    const response = await axios.get(BASE_URL)
+    return response.data
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const create = async (orderData) => {
+  try {
+    const response = await axios.post(BASE_URL, orderData, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}` // or wherever you store JWT
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error creating order:", error);
+    throw error;
+  }
+};
+
+export{
+  show,
+  create
+}
